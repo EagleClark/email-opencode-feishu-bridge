2068a2f58d555770467ff5960f367d75ec1349b0
EagleClark <eaglelljl@163.com>
2026-04-26 17:29:47 +0800

    first commit

---
+++ src/Router.tsx
@@ -0,0 +1,13 @@
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+import { HomePage } from './pages/Home.page';
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+]);
+
+export function Router() {
+  return <RouterProvider router={router} />;
+}
